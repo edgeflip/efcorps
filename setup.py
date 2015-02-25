f4c38d3006e656507eb47a2b29c996613df6184c
Tristan Crockett <tristan@edgeflip.com>
2015-02-25 12:21:51 -0600

    Forget installing ez_setup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from __future__ import with_statement
 import os.path
-import sys
 
 import ez_setup
 ez_setup.use_setuptools()
@@ -20,21 +19,6 @@ def get_long_description():
         return None
 
 
-def _is_install():
-    return len(sys.argv) >= 2 and sys.argv[1] == 'install'
-
-
-def get_py_modules():
-    """Modules to list.
-
-    Don't include `ez_setup` on install.
-
-    """
-    if _is_install():
-        return []
-    return ['ez_setup']
-
-
 def get_requirements(name):
     with open(os.path.join(ROOTDIR, 'requirements', name + '.txt')) as fh:
         return fh.read().splitlines()
@@ -45,7 +29,6 @@ setup(
     description="Shared models for various Edgeflip repos",
     long_description=get_long_description(),
     version='0.1',
-    py_modules=get_py_modules(),
     packages=['magnus'],
     install_requires=get_requirements('base'),
     maintainer="Edgeflip Developers",
